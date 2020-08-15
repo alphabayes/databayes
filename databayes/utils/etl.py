9e8ad5823c62b0734a6840c49f5ee3573eb12abe
Roland Donat <roland.donat@edgemind.net>
2020-08-15 15:47:32 +0200

    Add reges support to etl.discretize function

--- databayes/utils/etl.py
+++ databayes/utils/etl.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import re
 import pkg_resources
 installed_pkg = {pkg.key for pkg in pkg_resources.working_set}
 if 'ipdb' in installed_pkg:
     import ipdb
 
 
-def discretize(data_df, discretization_fun={},
-               discretization_fun_default=pd.cut,
-               discretization_params_default={"bins": 10}):
+def discretize(data_df, var_specs={},
+               fun_default=pd.cut,
+               params_default={"bins": 10},
+               var_specs_only=False):
     """ Auto discretize data. """
     data_ddf = data_df.copy(deep=True)
 
@@ -18,10 +20,21 @@ def discretize(data_df, discretization_fun={},
 
         if data_df[var].dtypes == "float":
 
-            disc_specs = discretization_fun.get(var, {})
-            disc_fun = disc_specs.get("fun", discretization_fun_default)
+            disc_specs = var_specs.get(var, {})
+            if len(disc_specs) == 0:
+                for pat, specs in var_specs.items():
+                    if re.search(pat, var):
+                        disc_specs = specs
+                        break
+
+            if len(disc_specs) == 0 and var_specs_only:
+                # Do not discretize if current var has no
+                # discretization specs specified
+                continue
+
+            disc_fun = disc_specs.get("fun", fun_default)
             disc_params = disc_specs.get(
-                "params", discretization_params_default)
+                "params", params_default)
 
             data_disc = disc_fun(data_df.loc[:, var],
                                  **disc_params)
